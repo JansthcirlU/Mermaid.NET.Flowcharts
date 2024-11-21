428443e49e063d47dd1031c767da6fff8658dad2
Jan Ulrichts <jan.ulrichts@gmail.com>
2024-11-21 23:39:27 +0100

    Added flowchart type with methods to add nodes and links.

---
+++ src/Mermaid.Flowcharts/Flowchart.cs
@@ -0,0 +1,61 @@
+using System.Text;
+
+namespace Mermaid.Flowcharts;
+
+public class Flowchart
+{
+    private readonly List<Node> _nodes = [];
+    private readonly List<Link> _links = [];
+
+    public string? Title { get; }
+
+    public Flowchart()
+    {
+
+    }
+    public Flowchart(string title)
+    {
+        Title = title;
+    }
+
+    public Flowchart AddNode(Node node)
+    {
+        if (!_nodes.Any(n => n.Id == node.Id)) _nodes.Add(node);
+        return this;
+    }
+
+    public Flowchart AddLink(Link link)
+    {
+        _links.Add(link);
+        return this;
+    }
+
+    public override string ToString()
+    {
+        StringBuilder flowchartStringBuilder = new();
+        if (Title is not null)
+        {
+            flowchartStringBuilder
+                .AppendLine("---")
+                .AppendLine($"title: {Title}")
+                .AppendLine("---");
+        }
+        flowchartStringBuilder.AppendLine("flowchart TD");
+
+        IEnumerable<Node> allNodes = _nodes
+            .Concat(_links.Select(link => link.Source))
+            .Concat(_links.Select(link => link.Destination))
+            .OrderBy(node => node.Id.Value)
+            .DistinctBy(node => node.Id);
+        foreach (Node node in allNodes)
+        {
+            flowchartStringBuilder.AppendLine($"    {node.ToString()}");
+        }
+        flowchartStringBuilder.AppendLine();
+        foreach (Link link in _links)
+        {
+            flowchartStringBuilder.AppendLine($"    {link.ToString()}");
+        }
+        return flowchartStringBuilder.ToString();
+    }
+}
