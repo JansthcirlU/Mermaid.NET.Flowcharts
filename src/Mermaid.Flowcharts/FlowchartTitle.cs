545c7f60d6062adc3aa18a3aefc656e1598b4ea9
Jan Ulrichts <jan.ulrichts@gmail.com>
2024-11-22 10:10:34 +0100

    Added flowchart title type with tests.

---
+++ src/Mermaid.Flowcharts/FlowchartTitle.cs
@@ -0,0 +1,32 @@
+namespace Mermaid.Flowcharts;
+
+public readonly struct FlowchartTitle
+{
+    public string Text { get; }
+
+    public FlowchartTitle()
+    {
+        throw new InvalidOperationException("You must create a flowchart title with a value.");
+    }
+    private FlowchartTitle(string text)
+    {
+        Text = text;
+    }
+
+    public static FlowchartTitle FromString(string text)
+    {
+        if (string.IsNullOrEmpty(text)) throw new ArgumentException("Flowchart title must not be null or empty.", nameof(text));
+        if (text.Contains('\n') || text.Contains('\r')) throw new ArgumentException("Flowchart title must not contain new lines.", nameof(text));
+        if (string.IsNullOrEmpty(text.Trim())) throw new ArgumentException("Flowchart title must not be whitespace.", nameof(text));
+        
+        return new(
+        $"""
+        ---
+        title: {text}
+        ---
+        """);
+    }
+
+    public override string ToString()
+        => Text;
+}
---
+++ tests/Mermaid.Flowcharts.Tests/FlowchartTitleTests.cs
@@ -0,0 +1,106 @@
+namespace Mermaid.Flowcharts.Tests;
+
+public class FlowchartTitleTests
+{
+    [Fact]
+    public void Title_ShouldThrow_WhenNewedWithDefaultConstructor()
+    {
+        // Act
+        InvalidOperationException? ex = Assert.Throws<InvalidOperationException>(
+            () => new FlowchartTitle()
+        );
+
+        // Assert
+        Assert.NotNull(ex);
+        Assert.StartsWith("You must create a flowchart title with a value.", ex.Message);
+    }
+
+    [Fact]
+    public void Title_ShouldThrow_WhenNull()
+    {
+        // Arrange
+        string text = null!;
+
+        // Act
+        ArgumentException? ex = Assert.Throws<ArgumentException>(
+            () => FlowchartTitle.FromString(text)
+        );
+
+        // Assert
+        Assert.NotNull(ex);
+        Assert.StartsWith("Flowchart title must not be null or empty.", ex.Message);
+    }
+
+    [Fact]
+    public void Title_ShouldThrow_WhenEmpty()
+    {
+        // Arrange
+        string text = string.Empty;
+
+        // Act
+        ArgumentException? ex = Assert.Throws<ArgumentException>(
+            () => FlowchartTitle.FromString(text)
+        );
+
+        // Assert
+        Assert.NotNull(ex);
+        Assert.StartsWith("Flowchart title must not be null or empty.", ex.Message);
+    }
+
+    [Theory]
+    [InlineData(" ")]
+    [InlineData("\t")]
+    public void Title_ShouldThrow_WhenWhitespace(string whitespace)
+    {
+        // Arrange
+        string text = whitespace;
+
+        // Act
+        ArgumentException? ex = Assert.Throws<ArgumentException>(
+            () => FlowchartTitle.FromString(text)
+        );
+
+        // Assert
+        Assert.NotNull(ex);
+        Assert.StartsWith("Flowchart title must not be whitespace.", ex.Message);
+    }
+
+    [Theory]
+    [InlineData("\n")]
+    [InlineData("\r")]
+    public void Title_ShouldThrow_WhenNewline(string newline)
+    {
+        // Arrange
+        string text = newline;
+
+        // Act
+        ArgumentException? ex = Assert.Throws<ArgumentException>(
+            () => FlowchartTitle.FromString(text)
+        );
+
+        // Assert
+        Assert.NotNull(ex);
+        Assert.StartsWith("Flowchart title must not contain new lines.", ex.Message);
+    }
+
+    [Theory]
+    [InlineData(
+        'a',
+        """
+        ---
+        title: a
+        ---
+        """)]
+    public void Title_ShouldToString_SingleLetter(char letter, string expected)
+    {
+        // Arrange
+        string text = letter.ToString();
+        FlowchartTitle title = FlowchartTitle.FromString(text);
+
+        // Act
+        string actual = title.ToString();
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+}
