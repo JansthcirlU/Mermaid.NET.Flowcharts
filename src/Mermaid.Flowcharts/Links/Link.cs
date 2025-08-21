e8ea48b3d6fbeafbda676e8924e5b4cb0a946bff
Jan Ulrichts <jan.ulrichts@gmail.com>
2025-08-21 08:10:32 +0200

    Renamed LinkStyle to LinkType.

--- src/Mermaid.Flowcharts/Links/Link.cs
+++ src/Mermaid.Flowcharts/Links/Link.cs
@@ -6,7 +6,7 @@ public readonly record struct Link : IMermaidPrintable
 {
     public readonly INode Source { get; }
     public readonly INode Destination { get; }
-    public readonly LinkStyle Style { get; }
+    public readonly LinkType Type { get; }
     public readonly LinkText? Text { get; }
 
     [Obsolete(error: true, message: $"Please use the factory methods instead of the default constructor to create a new {nameof(Link)}.")]
@@ -16,23 +16,23 @@ public readonly record struct Link : IMermaidPrintable
     private Link(
         INode source,
         INode destination,
-        LinkStyle style,
+        LinkType type,
         LinkText? text = null)
     {
         Source = source;
         Destination = destination;
-        Style = style;
+        Type = type;
         Text = text;
     }
 
-    public static Link Create(INode source, INode destination, LinkStyle? style = null, string? linkText = null)
-        => new(source, destination, style ?? LinkStyle.Create(), linkText is not null ? LinkText.FromString(linkText) : null);
+    public static Link Create(INode source, INode destination, LinkType? type = null, string? linkText = null)
+        => new(source, destination, type ?? LinkType.Create(), linkText is not null ? LinkText.FromString(linkText) : null);
 
     public override string ToString()
         => ToMermaidString();
 
     public string ToMermaidString(int indentations = 0, string indentationText = "  ")
         => Text is null
-            ? $"{indentationText.Repeat(indentations)}{Source.Id} {Style} {Destination.Id}"
-            : $"{indentationText.Repeat(indentations)}{Source.Id} {Style}|{Text}| {Destination.Id}";
+            ? $"{indentationText.Repeat(indentations)}{Source.Id} {Type} {Destination.Id}"
+            : $"{indentationText.Repeat(indentations)}{Source.Id} {Type}|{Text}| {Destination.Id}";
 }
