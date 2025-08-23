22c6303668f5826196b32a020240d2a4e8134e4b
Jan Ulrichts <jan.ulrichts@gmail.com>
2025-08-23 19:59:20 +0200

    Removed dash array rendering exception for pixels.

--- src/Mermaid.Flowcharts/Styling/Attributes/DashArray.cs
+++ src/Mermaid.Flowcharts/Styling/Attributes/DashArray.cs
@@ -4,15 +4,6 @@ public record DashArray(IEnumerable<DashSize> DashSizes) : IMermaidStyle
 {
     public string ToMermaidString()
         => DashSizes.Any()
-            ? $"stroke-dasharray: {string.Join(' ', DashSizes.Select(ds =>
-            {
-                string mermaid = ds.ToMermaidString();
-
-                if (ds is DashSize.LengthDashSize lds && lds.LengthSize.Unit == Enums.Unit.Px)
-                {
-                    return mermaid[..^2]; // Omit 'px' unit because it is implied/default
-                }
-                return mermaid;   
-            }))}"
+            ? $"stroke-dasharray: {string.Join(' ', DashSizes.Select(ds => ds.ToMermaidString()))}"
             : "stroke-dasharray:none";
 }
