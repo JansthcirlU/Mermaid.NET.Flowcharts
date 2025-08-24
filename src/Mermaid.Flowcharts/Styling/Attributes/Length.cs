42cae8a9fb769d51ffe9a35735a1272807c9552d
Jan Ulrichts <jan.ulrichts@gmail.com>
2025-08-24 11:03:16 +0200

    Refactored length to disallow NaN.

--- src/Mermaid.Flowcharts/Styling/Attributes/Length.cs
+++ src/Mermaid.Flowcharts/Styling/Attributes/Length.cs
@@ -1,11 +1,22 @@
-using System.Globalization;
+using Mermaid.Flowcharts.Numerical;
 using Mermaid.Flowcharts.Styling.Attributes.Base;
 using Mermaid.Flowcharts.Styling.Attributes.Enums;
 
 namespace Mermaid.Flowcharts.Styling.Attributes;
 
-public readonly record struct Length(double Value, Unit Unit) : ICssAttribute
+public readonly record struct Length : ICssAttribute
 {
+    public double Value { get; }
+    public Unit Unit { get; }
+    
+    public Length(double value, Unit unit)
+    {
+        if (double.IsNaN(value)) throw new ArgumentOutOfRangeException(nameof(value), "Length must be a real number.");
+
+        Value = value;
+        Unit = unit;
+    }
+
     public string ToCss()
-        => $"{Value.ToString("0.###", CultureInfo.InvariantCulture)}{Unit.ToUnitString()}";
+        => $"{Value.ToNumberString()}{Unit.ToUnitString()}";
 }
