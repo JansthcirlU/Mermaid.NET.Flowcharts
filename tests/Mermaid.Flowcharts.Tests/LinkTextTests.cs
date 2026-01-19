2b10c0063ec7066e71fe823e841aa41dd01ce5b9
Jan Ulrichts <jan.ulrichts@gmail.com>
2026-01-20 00:59:28 +0100

    Refactored link text tests to quote the text by default and allow special characters.

--- tests/Mermaid.Flowcharts.Tests/LinkTextTests.cs
+++ tests/Mermaid.Flowcharts.Tests/LinkTextTests.cs
@@ -37,45 +37,19 @@ public class LinkTextTests
     }
 
     [Theory]
-    [InlineData("\n")]
-    [InlineData("\r")]
-    public void LinkText_ShouldThrow_WhenNewline(string newline)
-    {
-        // Arrange
-        string text = newline;
-
-        // Act
-        ArgumentException? ex = Assert.Throws<ArgumentException>(
-            () => LinkText.FromString(text)
-        );
-
-        // Assert
-        Assert.NotNull(ex);
-        Assert.StartsWith("Link text must not contain new lines.", ex.Message);
-    }
-
-    [Theory]
-    [InlineData("\"")]
-    [InlineData("|")]
-    public void LinkText_ShouldThrow_WhenContainsEscapeCharacter(string escapeCharacter)
-    {
-        // Arrange
-        string text = escapeCharacter;
-
-        // Act
-        ArgumentException? ex = Assert.Throws<ArgumentException>(
-            () => LinkText.FromString(text)
-        );
-
-        // Assert
-        Assert.NotNull(ex);
-        Assert.StartsWith($"Link text must not contain illegal character \"{escapeCharacter}\".", ex.Message);
-    }
-
-    [Theory]
-    [InlineData("text", 2, "  ", "    text")]
-    [InlineData("mermaid is fun", 1, " ", " mermaid is fun")]
-    public void ToMermaidString_WhenIndentations(string text, int indentations, string indentationText, string expected)
+    [InlineData("text", 2, "  ", "\"text\"")]
+    [InlineData("mermaid is fun", 1, " ", "\"mermaid is fun\"")]
+    [InlineData("text with () parentheses", 2, "  ", "\"text with () parentheses\"")]
+    [InlineData(
+        """
+        text with
+            new line
+        """, 2, "  ",
+        """
+        "text with
+            new line"
+        """)]
+    public void ToMermaidString_ShouldIgnoreIndentationsAndAlwaysEscape(string text, int indentations, string indentationText, string expected)
     {
         // Arrange
         LinkText linkText = LinkText.FromString(text);
