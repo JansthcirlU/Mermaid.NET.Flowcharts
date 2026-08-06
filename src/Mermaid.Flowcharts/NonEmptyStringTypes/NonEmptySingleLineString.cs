d7a048fb6d06ed74ab0b05c6663b89c930a078f5
Jan Ulrichts <jan.ulrichts@gmail.com>
2026-08-06 18:20:58 +0200

    Enforced factory methods for non-empty string types.

--- src/Mermaid.Flowcharts/NonEmptyStringTypes/NonEmptySingleLineString.cs
+++ src/Mermaid.Flowcharts/NonEmptyStringTypes/NonEmptySingleLineString.cs
@@ -7,18 +7,27 @@ public readonly record struct NonEmptySingleLineString
     public static readonly SearchValues<char> NewLineSearchValues = SearchValues.Create("\n\r\u2028\u2029\u0085");
     public NonEmptyString Value { get; }
 
-    public NonEmptySingleLineString(NonEmptyString value)
+    [Obsolete(error: true, message: $"Please use the factory methods instead of the default constructor to create a new {nameof(NonEmptySingleLineString)}.")]
+#pragma warning disable CS8618 // This constructor is never used
+    public NonEmptySingleLineString() { }
+#pragma warning restore CS8618
+    private NonEmptySingleLineString(NonEmptyString value)
     {
-        if (value.Value.AsSpan().IndexOfAny(NewLineSearchValues) > -1)
-        {
-            throw new ArgumentException("Non-empty single line string must not contain any newline characters or carriage returns.", nameof(value));
-        }
-
         Value = value;
     }
 
+    public static NonEmptySingleLineString FromString(string s)
+    {
+        if (s.AsSpan().IndexOfAny(NewLineSearchValues) > -1)
+        {
+            throw new ArgumentException("Non-empty single line string must not contain any newline characters or carriage returns.", nameof(s));
+        }
+
+        NonEmptyString nonEmpty = NonEmptyString.FromString(s);
+        return new(nonEmpty);
+    }
+
     public static implicit operator string(NonEmptySingleLineString nesls) => nesls.Value;
-    public static implicit operator NonEmptySingleLineString(string s) => new(s);
 
     public override string ToString()
         => Value;
--- src/Mermaid.Flowcharts/NonEmptyStringTypes/NonEmptyString.cs
+++ src/Mermaid.Flowcharts/NonEmptyStringTypes/NonEmptyString.cs
@@ -2,20 +2,29 @@ namespace Mermaid.Flowcharts.NonEmptyStringTypes;
 
 public readonly record struct NonEmptyString
 {
-    public string Value { get; }
+    private readonly string _value;
+    public string Value => _value ?? throw new InvalidOperationException($"{nameof(NonEmptyString)} value must never be null. Make sure you use the {nameof(FromString)} factory method to construct a new {nameof(NonEmptyString)}.");
 
-    public NonEmptyString(string value)
+    [Obsolete(error: true, message: $"Please use the factory methods instead of the default constructor to create a new {nameof(NonEmptyString)}.")]
+#pragma warning disable CS8618 // This constructor is never used
+    public NonEmptyString() { }
+#pragma warning restore CS8618
+    private NonEmptyString(string value)
     {
-        if (string.IsNullOrWhiteSpace(value))
+        _value = value;
+    }
+
+    public static NonEmptyString FromString(string s)
+    {
+        if (string.IsNullOrWhiteSpace(s))
         {
-            throw new ArgumentException("Non-empty string must not be null or empty or whitespace.", nameof(value));
+            throw new ArgumentException("Non-empty string must not be null or empty or whitespace.", nameof(s));
         }
 
-        Value = value;
+        return new(s);
     }
 
     public static implicit operator string(NonEmptyString nes) => nes.Value;
-    public static implicit operator NonEmptyString(string s) => new(s);
 
     public override string ToString()
         => Value;
