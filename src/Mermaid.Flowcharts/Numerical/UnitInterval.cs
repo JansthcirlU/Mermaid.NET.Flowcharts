e711c8eda13df097444564980c9082cb068801e2
Jan Ulrichts <jan.ulrichts@gmail.com>
2026-08-07 00:23:29 +0200

    Removed implicit conversion to double from UnitInterval.

--- src/Mermaid.Flowcharts/Numerical/UnitInterval.cs
+++ src/Mermaid.Flowcharts/Numerical/UnitInterval.cs
@@ -33,8 +33,6 @@ public readonly record struct UnitInterval : INumerical
         return new(value);
     }
 
-    public static implicit operator double(UnitInterval i) => i.Value;
-
     public string ToNumericalString()
         => Value.ToNumberString();
 }
